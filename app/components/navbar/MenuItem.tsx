27410942fa4279d9da3d9b93427673aa2f2e2827
bookhabit <qnrgoqlt@naver.com>
2023-07-17 03:22:49 +0900

    프로젝트 세팅 및 Navbar UI

---
+++ app/components/navbar/MenuItem.tsx
@@ -0,0 +1,21 @@
+'use client'
+
+interface MenuItemPros{
+    onClick:()=>void;
+    label:string;
+}
+
+const MenuItem:React.FC<MenuItemPros> = ({
+    onClick,label
+}) => {
+    return (
+        <div 
+            className="px-4 py-3 hover:bg-neutral-100 transition font-semibold"
+            onClick={onClick}
+            >
+            {label}
+        </div>
+    );
+};
+
+export default MenuItem;
