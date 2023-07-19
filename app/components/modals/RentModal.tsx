37f6bbd8414880fbe89759a7cacfe19ea4e1cda9
bookhabit <qnrgoqlt@naver.com>
2023-07-19 15:41:47 +0900

    숙소등록 - 이미지업로드

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -10,6 +10,7 @@ import { FieldValues, useForm } from "react-hook-form";
 import CountrySelect from "../inputs/CountrySelect";
 import dynamic from "next/dynamic";
 import Counter from "../inputs/Counter";
+import ImageUpload from "../inputs/ImageUpload";
 
 
 enum STEPS {
@@ -46,6 +47,7 @@ const RentModal = () => {
     const guestsCount = watch('guestCount')
     const roomCount = watch('roomCount')
     const bathroomCount = watch('bathroomCount')
+    const imageSrc = watch('imageSrc')
 
     const Map = useMemo(()=>dynamic(()=>import('../Map'),{
         ssr:false
@@ -150,6 +152,21 @@ const RentModal = () => {
                     onChange={(value)=>setCustomValue('bathroomCount',value)}
                 />
                 <hr/>
+            </div>
+        )
+    }
+
+    if(step===STEPS.IMAGES){
+        bodyContent=(
+            <div className="flex flex-col gap-8">
+                <Heading 
+                    title="Add a photo of your place"
+                    subTitle="Show guests what your place looks like"
+                />
+                <ImageUpload
+                    value={imageSrc}
+                    onChange={(value)=>setCustomValue('imageSrc',value)}
+                />
             </div>
         )
     }
