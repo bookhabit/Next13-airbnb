78a5f0aabcfcc4b4d51a33a174bb0982f7997ff8
bookhabit <qnrgoqlt@naver.com>
2023-07-20 09:16:01 +0900

    ListingCard 오류 해결

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -26,7 +26,7 @@ const ListingCard:React.FC<ListingCardProps> = ({
     const router = useRouter();
     const {getByValue} = useCountries();
     const location = getByValue(data.locationValue)
-    console.log(data)
+    
     const handleCancel = useCallback((e:React.MouseEvent<HTMLButtonElement>)=>{
         e.stopPropagation();
 
@@ -57,47 +57,50 @@ const ListingCard:React.FC<ListingCardProps> = ({
     },[reservation])
 
     return (
-        <div onClick={()=>router.push(`/listings/${data.id}`)} className="col-span-1 cursor-pointer group ">
-            <div className="flex flex-col gap-2 w-full">
-                <div className="aspect-square w-full relative overflow-hidden rounded-xl">
-                    <Image
-                        fill
-                        alt="Listing"
-                        src={data.imageSrc}
-                        className="object-cover h-full w-full group-hover:scale-110 transition"
-                    />
-                    <div className="absolute top-3 right-3">
-                        <HeartButton
-                            listingId={data.id}
-                            currentUser={currentUser}
-                        />
-                    </div>
-                    <div className="font-semibold text-lg">
-                        {location?.region},{location?.label}
-                    </div>
-                    <div className="font-light text-neutral-500">
-                        {reservationDate || data.category}
-                    </div>
-                    <div className="flex flex-row items-center gap-1">
-                        <div className="font-semibold">
-                            $ {price}
-                        </div>
-                        {!reservation && (
-                            <div className="font-light">night</div>
-                        )}
-                    </div>
-                    {onAction && actionLabel && (
-                        <Button 
-                            disabled={disabled}
-                            small
-                            label={actionLabel}
-                            onClick={handleCancel}
-                        />
-                    )}
-                </div>
+        <div 
+          onClick={() => router.push(`/listings/${data.id}`)} 
+          className="col-span-1 cursor-pointer group"
+        >
+          <div className="flex flex-col gap-2 w-full">
+            <div className="aspect-square w-full relative overflow-hidden rounded-xl">
+              <Image
+                fill
+                className="object-cover h-full w-full group-hover:scale-110 transition"
+                src={data.imageSrc}
+                alt="Listing"
+              />
+              <div className="absolute top-3 right-3">
+                <HeartButton 
+                  listingId={data.id} 
+                  currentUser={currentUser}
+                />
+              </div>
             </div>
+            <div className="font-semibold text-lg">
+              {location?.region}, {location?.label}
+            </div>
+            <div className="font-light text-neutral-500">
+              {reservationDate || data.category}
+            </div>
+            <div className="flex flex-row items-center gap-1">
+              <div className="font-semibold">
+                $ {price}
+              </div>
+              {!reservation && (
+                <div className="font-light">night</div>
+              )}
+            </div>
+            {onAction && actionLabel && (
+              <Button
+                disabled={disabled}
+                small
+                label={actionLabel} 
+                onClick={handleCancel}
+              />
+            )}
+          </div>
         </div>
-    );
+       );
 };
 
 export default ListingCard;
